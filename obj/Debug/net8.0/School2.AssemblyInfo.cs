c74fcd65e521f9d006d0ae8d86fb1638a07065cb
ViktorEngman <Viktor.trelleborg@gmail.com>
2024-09-25 11:07:55 +0200

    Skapat ignore och pusha svave

--- obj/Debug/net8.0/School2.AssemblyInfo.cs
+++ obj/Debug/net8.0/School2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df844a76e918f2ac08a2e4173db30d61b8c1bc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4252b16b115d6ca8b6e822f28e0774aab4b8a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("School2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
